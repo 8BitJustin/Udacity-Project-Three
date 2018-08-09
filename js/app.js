942ffedd77bab39c58ed70b191b4c3a4fa510b5b
Justin Olson <j.olson.digital@gmail.com>
2018-08-09 07:53:09 -0700

    Created variables for Enemy. Created Player class, with update, render and handle methods. Created variables for allEnemies, player and enemy, and altered keys to reflect ASDW

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function(x,y,spd) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.spd = spd;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    // Loops enemies from right to left
+    if (this.x >= 505) {
+        this.x = 0;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,23 +32,71 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function(x,y,spd) {
+    this.x = x;
+    this.y = y;
+    this.spd = spd;
+    this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function() {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress == 'left') {
+        player.x += -101;
+    }
+    if (keyPress == 'up') {
+        player.y -= 83;
+    }
+    if (keyPress == 'right') {
+        player.x += 101;
+    }
+    if (keyPress == 'down') {
+        player.y -= -83;
+    }
+
+    // Keep player within canvas
+    if (player.y > 383 ) {
+        player.y = 383;
+    }
+    if (player.x > 402.5) {
+        player.x = 402.5;
+    }
+    if (player.x < 2.5) {
+        player.x = 2.5;
+    }
+    if (player.y <= 0) {
+        player.x = 202.5;
+        player.y = 383;
+        alert('You won!');
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [];
+const player = new Player(202.5, 383, 50);
+const enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
 
-
+allEnemies.push(enemy);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        65: 'left',
+        87: 'up',
+        68: 'right',
+        83: 'down'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+
 });
