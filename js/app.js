3e33dbcd49d2e63d1588839cd1248dd5a02268e8
Justin Olson <j.olson.digital@gmail.com>
2018-08-10 09:19:57 -0700

    Completed enemy update so random speeds accur. Created collision function to run within enemy update. Updated enemyposition with foreach to create three enemies

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Enemies our player must avoid
+// Enemy class
 const Enemy = function(x,y,spd) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -8,12 +8,20 @@ const Enemy = function(x,y,spd) {
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
+// Updates the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function() {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Enemy.prototype.update = function(dt) {
+    // Sets initial speed at start
+    this.x += this.spd * dt;
+
+    // Gives random speed when x is greater than canvas width (restarts)
+    if (this.x > 550) {
+        this.x = -101;
+        this.spd = 150 + Math.floor(Math.random() * 350);
+    }
+
+    // Runs collision function, using 'this' as the argument
+    collision(this);
 }
 
 // Draw the enemy on the screen, required method for game
@@ -21,9 +29,8 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class, with required update(), render() and
+// a handleInput() methods.
 const Player = function(x,y,spd) {
     this.x = x;
     this.y = y;
@@ -69,34 +76,30 @@ Player.prototype.handleInput = function(keyPress) {
         alert('You won!');
     }
 
-    // Collision function
-    collision();
 }
 
-// Function to detect collisions
-const collision = function() {
-    if (player.x === enemy.x && player.y === enemy.y) {
+// Collision between player and enemy check (ran within Enemy.prototype.update)
+const collision = function(nme) {
+    if (player.x < nme.x + 80 && player.x + 80 > nme.x &&
+        player.y < nme.y + 60 && player.y + 60 > nme.y) {
         player.x = 202.5;
         player.y = 383;
     }
 }
 
+// Places all enemy objects in an array
+const allEnemies = [];
+// Places the player object in a variable called player
+const player = new Player(202.5, 383, 50);
 // Array with enemy positions
 const enemyPosition = [217, 134, 51];
-// Pulls one number by random to place as Y coordinate for enemy variable
-const randomItem = enemyPosition[Math.floor(Math.random()*enemyPosition.length)];
+// Loops through allEnemies array, puts three enemies on screen at different Y positions
+enemyPosition.forEach(function(posY) {
+    enemy = new Enemy(0, posY, 250);
+    allEnemies.push(enemy);
+});
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-const allEnemies = [];
-const player = new Player(202.5, 383, 50);
-const enemy = new Enemy(0, randomItem, 50);
-
-allEnemies.push(enemy);
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys the Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         65: 'left',
